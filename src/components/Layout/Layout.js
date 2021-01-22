accd83fad4bc76e00480aff129c4623ab68aa0ec
Jim Nguyen <vjimnguyen@gmail.com>
2021-01-21 20:16:08 -0800

    Finished base burger app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Layout.module.css";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => {
-  return (
-    <>
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-    </>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Toolbar show={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
+}
+
+export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import classes from "./SideDrawer.module.css";
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+
+const sideDrawer = (props) => {
+  const attachedClasses = [
+    classes.SideDrawer,
+    props.open ? classes.Open : classes.Closed,
+  ].join(" ");
+
+  return (
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default sideDrawer;
